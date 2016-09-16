f0a54cc00c8384e799a6c49220db22fcbecafcab
Wendell Adriel <wendelladriel.ti@gmail.com>
2016-09-16 14:35:32 +0100

    adding get specific todo and create new todos

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos =[
-  {
-    id : 1,
-    description : 'Meet mom for lunch',
-    completed : false
-  },
-  {
-    id : 2,
-    description : 'Go to market',
-    completed : false
-  },
-  {
-    id : 3,
-    description : 'Go to work',
-    completed : true
-  },
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Todo API Root');
@@ -29,7 +19,29 @@ app.get('/todos', function (req, res) {
 });
 
 app.get('/todos/:id', function (req, res) {
-  res.send(todos);
+  var todoId = parseInt(req.params.id);
+  var matchedTodo = _.findWhere(todos, {id : todoId});
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  }
+
+  res.status(404).send();
+});
+
+app.post('/todos', function (req, res) {
+  var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+  body.description = body.description.trim();
+
+  body.id = todoNextId++;
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, function () {
